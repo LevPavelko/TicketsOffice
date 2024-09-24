50b614bc994dd759d45443d0b4a9ab2d1c4beff3
LevPavelko <lev.pavelko.studies@gmail.com>
2024-09-24 11:08:28 +0200

    d

---
+++ src/main/java/com/example/demo/TicketsKassaApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsKassaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsKassaApplication.class, args);
+		System.out.println("s");
+	}
+
+}
